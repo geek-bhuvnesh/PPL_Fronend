f2ac828664a9d8740f6e763ee53544d1f1531d48
geek-bhuvnesh <geek.bhuvnesh@gmail.com>
2015-09-02 13:14:03 +0530

    add a error message in getcategory

--- public/screens/home/HomeController.js
+++ public/screens/home/HomeController.js
@@ -1,6 +1,11 @@
 PPL_Frontend.controller('HomeController',['$scope','$http','HomeDataService','localstorageFactory','$state',function($scope,$http,HomeDataService,localstorageFactory,$state){
  console.log("Inside Controller");
 	
+ $scope.options = {
+     ErrorMessage :"",
+     showError : false
+ }
+
  $scope.User_logged_in = true;
  console.log("1:",localstorageFactory.getUserData('userData').email);
  if(!localstorageFactory.getUserData('userData').email){
@@ -9,6 +14,7 @@ PPL_Frontend.controller('HomeController',['$scope','$http','HomeDataService','lo
     //$state.go('login');
  }
  console.log("is User_logged_in:" +$scope.User_logged_in);
+ 
  $scope.userData = localstorageFactory.getUserData('userData');
  console.log("userData in HomeController" ,$scope.userData);
 
@@ -17,7 +23,9 @@ PPL_Frontend.controller('HomeController',['$scope','$http','HomeDataService','lo
   $scope.categories = data;
 
  },function(err){
-
+   console.log("error:",err);
+   $scope.options.showError = true;
+   $scope.options.ErrorMessage = err.message
  })
 
  $scope.logout = function(){
