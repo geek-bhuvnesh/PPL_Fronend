0d06069a8b0b6a4c3829f210e47782224f04f8a9
geek-bhuvnesh <geek.bhuvnesh@gmail.com>
2015-09-02 13:06:48 +0530

    work for category rendering at homepage

--- public/screens/home/HomeController.js
+++ public/screens/home/HomeController.js
@@ -1,33 +1,24 @@
 PPL_Frontend.controller('HomeController',['$scope','$http','HomeDataService','localstorageFactory','$state',function($scope,$http,HomeDataService,localstorageFactory,$state){
-	console.log("Inside Controller");
-	/*$http.get('http://localhost:8000/home').success(function(data){
-        console.log("Data:",data);
-	});
-*/
-
+ console.log("Inside Controller");
+	
  $scope.User_logged_in = true;
  console.log("1:",localstorageFactory.getUserData('userData').email);
  if(!localstorageFactory.getUserData('userData').email){
- 	console.log("2");
+ 	  console.log("2");
     $scope.User_logged_in = false;
     //$state.go('login');
  }
  console.log("is User_logged_in:" +$scope.User_logged_in);
  $scope.userData = localstorageFactory.getUserData('userData');
  console.log("userData in HomeController" ,$scope.userData);
-/*$(document).ready(function() {
-	$('#rght_cat_bg').click(function() {
-			$('.rght_list').toggle("slide");
-	});
-});
-$(document).ready(function() {
-	$('#opn_cat_bg').click(function() {
-			$('.sub_dwn').toggle("slide");
-	});
-});
-*/
 
+ HomeDataService.getAllCategories().then(function(data){
+  console.log("Catgeory Data in Home Controller:" + JSON.stringify(data));
+  $scope.categories = data;
 
+ },function(err){
+
+ })
 
  $scope.logout = function(){
     console.log("Logout called");
--- public/screens/home/factory.js
+++ public/screens/home/factory.js
@@ -3,8 +3,13 @@ PPL_Frontend.factory("logout", ["$resource","pplconfig",function($resource,pplco
    }, {});
 }]);
 
+PPL_Frontend.factory("allCategories", ["$resource","pplconfig",function($resource,pplconfig) {
+    return $resource(pplconfig.url+":3000/getAllCategories", {
+   }, {});
+}]);
 
-PPL_Frontend.factory("HomeDataService", ["$http", "$q", "logout", function($http, $q, logout) {
+
+PPL_Frontend.factory("HomeDataService", ["$http", "$q", "logout","allCategories", function($http, $q, logout,allCategories) {
    var userData = {};
    var selected;
    return {
@@ -14,6 +19,25 @@ PPL_Frontend.factory("HomeDataService", ["$http", "$q", "logout", function($http
            try {
                logout
                    .get(function(resp) {
+                      userData = resp;
+                      defer.resolve(resp);
+                   },function(err) {
+                       userData = {};
+                       defer.reject({});
+                       console.log(err);
+                   });
+           } catch (e) {
+               console.log(e.stack);
+               defer.reject({});
+           }
+           return defer.promise;
+       },
+       getAllCategories: function(){
+         console.log("ALL Categories factory");
+           var defer = $q.defer();
+           try {
+               allCategories
+                   .query(function(resp) {
                       userData = resp;
                       defer.resolve(resp);
                    },function(err) {
